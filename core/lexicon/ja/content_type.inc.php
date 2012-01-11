70a68f5dd83360083f701fe349e96b3e1fdf0706
kogu <flogskin@gmail.com>
2012-01-11 13:45:07 +0900

    翻訳と修正

--- core/lexicon/ja/content_type.inc.php
+++ core/lexicon/ja/content_type.inc.php
@@ -8,11 +8,12 @@
  * @author Nick http://smallworld.west-tokyo.com
  * @author shimojo http://www.priqia.com/
  * @author yamamoto http://kyms.jp
+ * @author honda http://kogus.org
  */
 $_lang['binary'] = 'バイナリ';
-$_lang['binary_desc'] = 'ファイルはバイナリ、もしくはテキストのどちらですか？';
+$_lang['binary_desc'] = 'ファイルはバイナリデータですか？';
 $_lang['content_type'] = 'コンテンツタイプ';
-$_lang['content_type_desc'] = 'コンテンツタイプはリソースの具体的なファイルシステムタイプです。ここではそれを管理するだけでなく、ファイル拡張子と関連付けることができます。';
+$_lang['content_type_desc'] = 'コンテンツタイプはリソースの具体的なファイルシステムタイプです。ここではその管理だけでなく、ファイル拡張子との関連付けも可能です。';
 $_lang['content_type_err_nf'] = 'コンテンツタイプが見つかりません。';
 $_lang['content_type_err_nfs'] = 'ID [[+id]] のコンテンツタイプが見つかりません。';
 $_lang['content_type_err_ns'] = 'コンテンツタイプが指定されていません。';
@@ -22,7 +23,7 @@ $_lang['content_type_err_remove'] = 'コンテンツタイプの削除中にエ�
 $_lang['content_type_err_save'] = 'コンテンツタイプの保存中にエラーが発生しました。';
 $_lang['content_type_new'] = '新しいコンテンツタイプ';
 $_lang['content_type_remove'] = 'コンテンツタイプの削除';
-$_lang['content_type_remove_confirm'] = '本当にこのコンテンツタイプを削除しますか？';
+$_lang['content_type_remove_confirm'] = 'このコンテンツタイプを削除してよろしいですか？';
 $_lang['content_types'] = 'コンテンツタイプ';
 $_lang['file_extensions'] = 'ファイル拡張子';
-$_lang['file_extensions_desc'] = 'This is the specified file extension for this Content Type.';
+$_lang['file_extensions_desc'] = 'このコンテンツタイプに関連付ける拡張子を指定します。';
