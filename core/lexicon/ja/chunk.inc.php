b591f6fa5b265c585178dd93a07d4ef46776e0ca
honda <flogskin@gmail.com>
2012-01-12 10:33:27 +0900

    日本語訳を追加

--- core/lexicon/ja/chunk.inc.php
+++ core/lexicon/ja/chunk.inc.php
@@ -1,18 +1,19 @@
 <?php
 /**
- * Chunk Japanese lexicon topic
- *
- * @language ja
- * @package modx
- * @subpackage lexicon
- * @author Nick http://smallworld.west-tokyo.com
- * @author shimojo http://www.priqia.com/
- * @author yamamoto http://kyms.jp
- */
+* Chunk Japanese lexicon topic
+*
+* @language ja
+* @package modx
+* @subpackage lexicon
+* @author Nick http://smallworld.west-tokyo.com
+* @author shimojo http://www.priqia.com/
+* @author yamamoto http://kyms.jp
+* @author honda http://kogus.org
+*/
 $_lang['chunk'] = 'チャンク';
-$_lang['chunk_desc_category'] = 'The Category this Chunk belongs in.';
-$_lang['chunk_desc_description'] = 'A short description of this chunk.';
-$_lang['chunk_desc_name'] = 'The name of this chunk. It will be available using the [[$nameOfChunk]] tags.';
+$_lang['chunk_desc_category'] = 'このチャンクが属するカテゴリーを選択して下さい。';
+$_lang['chunk_desc_description'] = 'このチャンクの短い説明を入力してください。';
+$_lang['chunk_desc_name'] = 'このチャンクの名前を指定してください。[[$チャンク名]]というタグによってチャンクを使用します。';
 $_lang['chunk_code'] = 'チャンク コード (html)';
 $_lang['chunk_desc'] = '説明';
 $_lang['chunk_delete_confirm'] = 'このチャンクを本当に削除しますか？';
