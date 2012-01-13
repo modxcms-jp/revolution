bd482351dff41c87102970cbab31b5efbd4c8291
yama <yamamoto@kyms.ne.jp>
2012-01-13 16:16:50 +0900

    コア用言語ファイルを最新状態にマージ(1)

--- core/lexicon/ja/element.inc.php
+++ core/lexicon/ja/element.inc.php
@@ -11,6 +11,7 @@
 $_lang['element'] = 'エレメント';
 $_lang['element_err_nf'] = 'エレメントが見つかりません。';
 $_lang['element_err_ns'] = 'エレメントを特定できません。';
+$_lang['element_static_source_immutable'] = 'The static file specified as the element source is not writable! You cannot edit the content of this element in the manager.';
 $_lang['is_static'] = 'Is Static';
 $_lang['is_static_msg'] = 'Indicates if the element source is stored in an external file.';
 $_lang['quick_create'] = 'クイック作成';
@@ -26,6 +27,8 @@ $_lang['quick_update_template'] = 'テンプレートをクイック編集';
 $_lang['quick_update_tv'] = 'テンプレート変数をクイック編集';
 $_lang['properties_export'] = 'エクスポート';
 $_lang['properties_import'] = 'プロパティをインポート';
+$_lang['property_preprocess'] = 'Pre-process tags in Property Values';
+$_lang['property_preprocess_msg'] = 'If enabled, tags in Default Property/Property Set values will be processed before they are used for Element processing.';
 $_lang['static_file'] = 'Static File';
 $_lang['static_file_msg'] = 'An external file location where the element source is stored.';
 $_lang['static_source'] = 'Media Source for Static File';
