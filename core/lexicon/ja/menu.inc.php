bdb4c57a60009c6dc8aef91900480fd6525f62ec
yama <yamamoto@kyms.ne.jp>
2012-08-07 08:27:27 +0900

    日本語訳を追加・一部調整

--- core/lexicon/ja/menu.inc.php
+++ core/lexicon/ja/menu.inc.php
@@ -5,15 +5,15 @@
  * @language ja
  * @package modx
  * @subpackage lexicon
+ * @author yamamoto http://kyms.jp 2012-08-06
  * @author Nick http://smallworld.west-tokyo.com
  * @author shimojo http://www.priqia.com/
- * @author yamamoto http://kyms.jp
  */
-$_lang['action'] = 'Action';
-$_lang['action_desc'] = 'The controller path to use for this menu item. The path to the controller is built by prefixing the Namespace path, controllers, and manager theme with this value. (Ex: user/update for core Namespace goes to [core_namespace_path]controllers/ [mgr_theme]/user/update.class.php )';
+$_lang['action'] = 'アクション';
+$_lang['action_desc'] = 'メニューアイテムとコントローラを関連づけて、アクションを作成します。The path to the controller is built by prefixing the Namespace path, controllers, and manager theme with this value. (Ex: user/update for core Namespace goes to [core_namespace_path]controllers/ [mgr_theme]/user/update.class.php )';
 $_lang['description_desc'] = 'The text, or lexicon key, that will be used for rendering the description text of this page in the menu.';
-$_lang['handler'] = 'Handler';
-$_lang['handler_desc'] = '(Optional) If set, will not use the action field, but instead run this Javascript instead when the menu item is clicked.';
+$_lang['handler'] = 'ハンドラー';
+$_lang['handler_desc'] = '(オプション) ここに入力された文字列がJavaScriptとしてメニューから実行されます。(例：location.href="http://modx.jp";)';
 $_lang['lexicon_key'] = 'レキシコンキー';
 $_lang['lexicon_key_desc'] = 'The text, or lexicon key, that will be used for rendering the title text of this page in the menu.';
 $_lang['menu_create'] = 'メニュー作成';
@@ -29,12 +29,12 @@ $_lang['menu_parent_err_nf'] = '親メニューが見つかりません。';
 $_lang['menu_remove'] = 'メニューアイテムを削除';
 $_lang['menu_top'] = 'トップメニュー';
 $_lang['menu_update'] = 'メニューアイテムを編集';
-$_lang['menus'] = 'Menus';
-$_lang['namespace'] = 'Namespace';
+$_lang['menus'] = 'メニュー';
+$_lang['namespace'] = 'ネームスペース';
 $_lang['namespace_desc'] = 'The Namespace which this menu item is based on. This will determine the path for the controller that is loaded.';
-$_lang['parameters'] = 'Parameters';
+$_lang['parameters'] = 'パラメータ';
 $_lang['parameters_desc'] = 'Any request parameters you want appended to the result URL when clicking this menu. (Ex: &expire=1)';
-$_lang['permissions'] = 'Permission';
+$_lang['permissions'] = 'パーミッション';
 $_lang['permissions_desc'] = 'A permission key required to load this menu item.';
 $_lang['topmenu'] = 'トップメニュー';
-$_lang['topmenu_desc'] = '管理画面の上部に表示されているメニューバーと、メニューアイテムの関連付けを行うことができます。メニュー内の任意の位置にアクションを配置するだけで簡単にメニューを編集できます。';
+$_lang['topmenu_desc'] = '管理画面の上部に表示されているトップメニューと、メニューアイテムの関連付けを行うことができます。コントローラ(アクション)を持たない操作を割り当てることもできます。';
