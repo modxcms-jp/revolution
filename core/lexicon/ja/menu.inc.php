d8c72cba011ddc46da876e4b80c176d9d7e3b18c
honda <flogskin@gmail.com>
2012-08-17 13:19:24 +0900

    日本語訳を追加。

--- core/lexicon/ja/menu.inc.php
+++ core/lexicon/ja/menu.inc.php
@@ -5,17 +5,18 @@
  * @language ja
  * @package modx
  * @subpackage lexicon
+ * @author honda http://kogus.org 2012-08-17
  * @author yamamoto http://kyms.jp 2012-08-11
  * @author Nick http://smallworld.west-tokyo.com
  * @author shimojo http://www.priqia.com/
  */
 $_lang['action'] = 'アクション(コントローラ)';
 $_lang['action_desc'] = 'コントローラにアクションIDを割り当てます。The path to the controller is built by prefixing the Namespace path, controllers, and manager theme with this value. (Ex: user/update for core Namespace goes to [core_namespace_path]controllers/ [mgr_theme]/user/update.class.php )';
-$_lang['description_desc'] = 'The text, or lexicon key, that will be used for rendering the description text of this page in the menu.';
-$_lang['handler'] = 'ハンドラー(アクションより優先されます)';
+$_lang['description_desc'] = 'メニュー内でこのページの説明に使用されるテキスト、またはレキシコンキー。';
+$_lang['handler'] = 'ハンドラー（アクションより優先されます）';
 $_lang['handler_desc'] = '(オプション) ここに入力された文字列がJavaScriptとしてメニューから実行されます。(例：location.href="http://modx.jp";)';
-$_lang['lexicon_key'] = 'レキシコンキー(アイテム名を直接入力することもできます)';
-$_lang['lexicon_key_desc'] = 'The text, or lexicon key, that will be used for rendering the title text of this page in the menu.';
+$_lang['lexicon_key'] = 'レキシコンキー（アイテム名を直接入力することもできます）';
+$_lang['lexicon_key_desc'] = 'メニューでこのページのタイトルに使用されるテキスト、またはレキシコンキー。';
 $_lang['menu_create'] = 'メニューアイテム作成';
 $_lang['menu_confirm_remove'] = 'このアクションをメニューから削除しますか？';
 $_lang['menu_err_ae'] = 'この名前は既に使用されています。他の名前を指定してください。';
@@ -31,10 +32,10 @@ $_lang['menu_top'] = 'トップメニュー';
 $_lang['menu_update'] = 'メニューアイテムを編集';
 $_lang['menus'] = 'メニュー';
 $_lang['namespace'] = 'ネームスペース';
-$_lang['namespace_desc'] = 'The Namespace which this menu item is based on. This will determine the path for the controller that is loaded.';
-$_lang['parameters'] = 'パラメータ(コントローラ呼び出し文字列の末尾に付加)';
-$_lang['parameters_desc'] = 'Any request parameters you want appended to the result URL when clicking this menu. (Ex: &expire=1)';
+$_lang['namespace_desc'] = 'このメニュー項目が基づく名前空間。ロードされるコントローラーのパスを決定します。';
+$_lang['parameters'] = 'メニュー項目のパラメータ';
+$_lang['parameters_desc'] = 'このメニュー項目のクリック時に渡したい任意のパラメーター。コントローラ呼び出し文字列の末尾に付加されます。（例： &expire=1）';
 $_lang['permissions'] = 'パーミッション';
-$_lang['permissions_desc'] = 'A permission key required to load this menu item.';
+$_lang['permissions_desc'] = 'このメニュー項目のロードに必要なパーミッションキー。';
 $_lang['topmenu'] = 'トップメニュー';
 $_lang['topmenu_desc'] = '管理画面のトップメニュー構成をマウス操作でカスタマイズできます。';
