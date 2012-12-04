3598c269a1df4c5de8f735e8ceb7d0d3785be8d9
yama <yamamoto@kyms.ne.jp>
2012-12-04 12:53:07 +0900

    レキシコンエントリー調整
    2.3相当の内容をいったん2.2.x系に戻す

--- core/lexicon/ja/menu.inc.php
+++ core/lexicon/ja/menu.inc.php
@@ -10,14 +10,8 @@
  * @author Nick http://smallworld.west-tokyo.com
  * @author shimojo http://www.priqia.com/
  */
-$_lang['action'] = 'アクション(コントローラ)';
-$_lang['action_desc'] = 'コントローラにアクションIDを割り当てます。The path to the controller is built by prefixing the Namespace path, controllers, and manager theme with this value. (Ex: user/update for core Namespace goes to [core_namespace_path]controllers/ [mgr_theme]/user/update.class.php )';
-$_lang['description_desc'] = 'メニュー内でこのページの説明に使用されるテキスト、またはレキシコンキー。';
-$_lang['handler'] = 'ハンドラー（アクションより優先されます）';
-$_lang['handler_desc'] = '(オプション) ここに入力された文字列がJavaScriptとしてメニューから実行されます。(例：location.href="http://modx.jp";)';
-$_lang['lexicon_key'] = 'レキシコンキー（アイテム名を直接入力することもできます）';
-$_lang['lexicon_key_desc'] = 'メニューでこのページのタイトルに使用されるテキスト、またはレキシコンキー。';
-$_lang['menu_create'] = 'メニューアイテム作成';
+$_lang['lexicon_key'] = 'レキシコンキー';
+$_lang['menu_create'] = 'メニュー作成';
 $_lang['menu_confirm_remove'] = 'このアクションをメニューから削除しますか？';
 $_lang['menu_err_ae'] = 'この名前は既に使用されています。他の名前を指定してください。';
 $_lang['menu_err_nf'] = 'メニューが見つかりませんでした。';
@@ -30,12 +24,5 @@ $_lang['menu_parent_err_nf'] = '親メニューが見つかりません。';
 $_lang['menu_remove'] = 'メニューアイテムを削除';
 $_lang['menu_top'] = 'トップメニュー';
 $_lang['menu_update'] = 'メニューアイテムを編集';
-$_lang['menus'] = 'メニュー';
-$_lang['namespace'] = 'ネームスペース';
-$_lang['namespace_desc'] = 'このメニュー項目が基づくネームスペース。ロードされるコントローラーのパスを決定します。';
-$_lang['parameters'] = 'メニュー項目のパラメータ';
-$_lang['parameters_desc'] = 'このメニュー項目のクリック時に渡したい任意のパラメーター。コントローラ呼び出し文字列の末尾に付加されます。（例： &expire=1）';
-$_lang['permissions'] = 'パーミッション';
-$_lang['permissions_desc'] = 'このメニュー項目のロードに必要なパーミッションキー。';
 $_lang['topmenu'] = 'トップメニュー';
 $_lang['topmenu_desc'] = '管理画面のトップメニュー構成をマウス操作でカスタマイズできます。';
